880e63f47cd008ab1a265de7e87fe303b8164ae8
Sabin Regmi <get2sabin@gmail.com>
2023-02-16 19:02:19 -0500

    Seperated User Methods

---
+++ scripts/user.js
@@ -0,0 +1,48 @@
+function loadInitialData() {
+  var users = [
+    {
+      id: 1,
+      name: "Sabin Regmi",
+      email: "get2sabin@gmail.com",
+      password: "123456",
+      type: "user",
+    },
+    {
+      id: 2,
+      name: "Admin",
+      email: "admin@admin.com",
+      password: "admin",
+      type: "admin",
+    },
+  ];
+  // saving to local storage
+  localStorage.setItem("users", JSON.stringify(users));
+}
+
+function checkIfUserExists() {
+  if (localStorage.getItem("users") === null) {
+    loadInitialData();
+  }
+}
+
+function validateUser(email, password) {
+  var users = JSON.parse(localStorage.getItem("users"));
+
+  var user = users.find(function (user) {
+    return user.email === email && user.password === password;
+  });
+
+  if (user) {
+    localStorage.setItem("currentUser", JSON.stringify(user));
+    localStorage.setItem("projects", JSON.stringify([]));
+
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Invalid email or password");
+  }
+}
+
+function getCurrentUser() {
+  var currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  return currentUser;
+}
