8394127517e3df485d570a9a7a8191ec85022c1b
Sabin Regmi <get2sabin@gmail.com>
2023-02-22 21:18:32 -0500

    Cleaned Up the Code A Bit

--- scripts/user.js
+++ scripts/user.js
@@ -17,7 +17,21 @@ function loadInitialData() {
     {
       id: 3,
       name: "Namrata Barot",
-      email: "namrata.barot3@gmail.com",
+      email: "namrata@gmail.com",
+      password: "123456",
+      type: "user",
+    },
+    {
+      id: 4,
+      name: "Olllaliuuuwaa",
+      email: "sayo@gmail.com",
+      password: "123456",
+      type: "user",
+    },
+    {
+      id: 3,
+      name: "Devanshi Gandhi",
+      email: "devanshi@gmail.com",
       password: "123456",
       type: "user",
     },
